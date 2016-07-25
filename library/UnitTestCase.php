8bf62a1426df04238ef70aebe706804f8ee9fa7f
sebbbbauer <i@sebastianbauer.name>
2016-07-25 12:03:26 +0200

    ESDEV-3518 Add hint comment about the usage of the protected access methods.

--- library/UnitTestCase.php
+++ library/UnitTestCase.php
@@ -811,6 +811,9 @@ abstract class UnitTestCase extends BaseTestCase
     /**
      * Set a given protected property of a given class instance to a given value.
      *
+     * Note: Please use this methods only for static 'mocking' or with other hard reasons!
+     *       For the most possible non static usages there exist other solutions.
+     *
      * @param object $classInstance Instance of the class of which the property will be set
      * @param string $property      Name of the property to be set
      * @param mixed  $value         Value to which the property will be set
@@ -829,6 +832,9 @@ abstract class UnitTestCase extends BaseTestCase
     /**
      * Get a given protected property of a given class instance.
      *
+     * Note: Please use this methods only for static 'mocking' or with other hard reasons!
+     *       For the most possible non static usages there exist other solutions.
+
      * @param object $classInstance Instance of the class of which the property will be set
      * @param string $property      Name of the property to be retrieved
      *
